4a4ac81ee18e77d384908419e8876aca159a71ac
Gautam Pillai <pillai.gautamh@gmail.com>
2025-11-11 19:28:24 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("denominator can't be 0")
+    return b / a # raise ZeroDivisionError if a == 0
+
+def log(a, b):
+    if a <= 1:
+        raise ValueError("Base has to be greater than 1")
+    return math.log(b, a)# use math library + raise ValueError
+
+def exp(a, b):
+    return a**b
+
+print(div(0,1))
