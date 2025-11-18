731d3df4123f09f31c72aecff7d4819957a0da89
Priya Schramm <schrammfamily@MacBook-Air-249.local>
2025-11-18 15:47:19 -0500

    modified test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -2,13 +2,15 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(add(1,2), 3)
+        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(-2, 5), 3)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(sub(-1, -1), 0)
+        self.assertEqual(sub(0, 0), 0)
+        self.assertEqual(sub(5, 2), 3)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -18,21 +20,22 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 10)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(2, 8), 3)
+        self.assertEqual(log(10, 100), 2)
+        self.assertEqual(log(3, 27), 3)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
+
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(-1, 10)
+        with self.assertRaises(ValueError):
+            log(1, -10)
+
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
@@ -51,6 +54,5 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     ##########################
 
-# Do not touch this
 if __name__ == "__main__":
     unittest.main()
